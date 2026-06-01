74c7e62b1308ab490821730c3d9ff9e0b1cb6cb7
gabrielapolacov <gabrielamorenopolacov@gmail.com>
2026-06-01 20:17:09 -0300

    Added random_number_amount function

--- generator.py
+++ generator.py
@@ -10,8 +10,58 @@ def load_subcategories(path):
             subcategories.append(row["Subcategory"])
     return subcategories
 
-if __name__ == "__main__":
-    subs = load_subcategories("data/subcategories.csv")
-    print(subs[:5])   # first 5 only
-    print(f"Total: {len(subs)}")
+def random_number_amount(): 
+    gross = round(random.uniform(50, 10000000), 2)
+    exempt = round(random.uniform(50, gross), 2)
+    taxable = round(gross - exempt), 2
+
+    return gross, exempt, taxable
+
+
+
+def make_excel(num_transaction, state, subcategory, store_id):
+
+    wb = Workbook()
+    ws = wb.active
+    ws.title = "Data import 1"
+
+
+    headers = [
+        "Store Id",
+        "State",
+        "County",
+        "City",
+        "Zip Code",
+        "Subcategory",
+        "Gross Sales"
+        "Exempt sales",
+        "Taxable sales",
+        "Tax Collected",
+        "Taxable purchases",
+        "Use tax accrued",
+    ]
+    ws.append(headers)
+
+    for t in range(num_transaction):
+        row = [
+            store_id,              # E-commerce → blank Store Id
+            state,
+            "Los Angeles",
+            "Los Angeles",
+            "90001",
+            subcategory,
+            100.00,
+            20.00,
+            80.00,
+            6.40,
+            0.00,
+            0.00,
+            0.00,
+        ]
+        ws.append(row)
+
+
+    output_name = "BasicAvalara_test.xlsx"
+    wb.save(output_name)
+    print(f"Created {output_name}")
 
